c52c42e399edd126cee5fe14bd906c227c1d4303
Mateus Castro <105495280+mateuscastro93@users.noreply.github.com>
2023-04-15 13:03:51 -0300

    Calculadora-imc
    calcula o imc

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+peso = float(input('Informe seu peso atual :'))
+altura = float(input('Informe sua altura : '))
+
+imc = peso / (altura**2)
+
+if imc < 18.5:
+    print('Abaixo do peso')
+elif 18.5 <= imc < 25:
+    print('Peso ideal')
+elif 25 <= imc < 30 :
+    print('Sobre peso')
+elif 30 <= imc < 40 :
+    print('Obesidade')
+elif imc >= 40:
+    print('Obesidade Mórbida')
+
+
